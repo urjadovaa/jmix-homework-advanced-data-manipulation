0fb6915ae8477d504c0a839dd81c916c5516ed44
Urjadovaa <urjadovaa@gmail.com>
2023-05-25 13:11:31 +0300

    Homework done!

--- src/main/java/com/sample/hotel/screen/roomreservation/ReservedRoomsScreen.java
+++ src/main/java/com/sample/hotel/screen/roomreservation/ReservedRoomsScreen.java
@@ -2,6 +2,10 @@ package com.sample.hotel.screen.roomreservation;
 
 import com.sample.hotel.entity.Client;
 import com.sample.hotel.entity.RoomReservation;
+import io.jmix.core.DataManager;
+import io.jmix.core.FetchPlan;
+import io.jmix.core.FetchPlans;
+import io.jmix.core.Id;
 import io.jmix.ui.Dialogs;
 import io.jmix.ui.action.Action;
 import io.jmix.ui.component.GroupTable;
@@ -16,13 +20,24 @@ public class ReservedRoomsScreen extends StandardLookup<RoomReservation> {
     private GroupTable<RoomReservation> roomReservationsTable;
     @Autowired
     private Dialogs dialogs;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private FetchPlans fetchPlans;
 
     @Subscribe("roomReservationsTable.viewClientEmail")
     public void onRoomReservationsTableViewClientEmail(Action.ActionPerformedEvent event) {
+
         RoomReservation reservation = roomReservationsTable.getSingleSelected();
-        if (reservation == null) {
-            return;
-        }
+        if (reservation == null) return;
+
+        // перезагрузка сущности Client с нужным атрибутом
+        FetchPlan clientEmailFetchPlan = fetchPlans.builder(RoomReservation.class)
+                .add("id")
+                .add("booking.client.email")
+                .build();
+        reservation = dataManager.load(Id.of(reservation)).fetchPlan(clientEmailFetchPlan).one();
+
         Client client = reservation.getBooking().getClient();
 
         dialogs.createMessageDialog()
